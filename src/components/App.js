8a42fec5fc6a07700f9ea53dd1873ed26eca64d1
eustachekamala <eustachejared25@gmail.com>
2024-08-17 11:04:49 +0300

    I added the filtered by name and by weight

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,77 @@
 import React from "react";
 import Nav from "./Nav";
+import { useState } from "react";
 
 import hogs from "../porkers_data";
 
+
 function App() {
+
+const [porkers, setPorkers] = useState(hogs);
+const [weight, setWeight] = useState(hogs);
+
+function handleClick(e) {
+	e.preventDefault();
+	setPorkers(hogs);
+}
+
+
+function filterPorkers(porkers, search) {
+	if (search === "") {
+		return porkers;
+	}
+	const filteredPorkers = porkers.filter((porker) => {
+		return porker.name.toLowerCase().includes(search.toLowerCase());
+	});
+	return filteredPorkers;
+}
+
+const filterPorkersByWeight = (porkers, weight) => {
+	if (weight === "") {
+		return porkers;
+	}
+	const filteredPorkers = porkers.filter((porker) => {
+		return porker.weight.toLowerCase().includes(weight.toLowerCase());
+	});
+	return filteredPorkers;
+};
+
+	function handleSearch(e) {
+		e.preventDefault();
+		const search = e.target.value;
+		setPorkers(filterPorkers(porkers, search));
+
+	}
+
+	function handleWeightSearch(e) {
+		e.preventDefault();
+		const weight = e.target.value;
+		setPorkers(filterPorkersByWeight(porkers, weight));
+
+	}
 
 	return (
 		<div className="App">
 			<Nav />
-			<div className="porkers">
-				{hogs.map((hog, index) => (
+			<div className="search-wrapper">
+				<input type="text" onChange={handleSearch} placeholder="Search..." />
+				<input type="text" onChange={handleWeightSearch} placeholder="Weight..." />
+			</div>
+			<div className="porkers" >
+				{porkers.map((hog, index) => (
 					<div key={index} className="porker">
-						<div className="porker-name">{hog.name}</div>
-						<div className="porker-hogs">{hog.hogs}</div>
-						<div className="porker-price">{hog.price}</div>
+							<img className="porker-image" onClick={handleClick} src={hog.image} alt={hog.name} />
+							<div className="porker-info">
+								<p>{hog.name}</p>
+								<p>{hog.specialty}</p>
+								<p>Weight: {hog.weight}</p>
+							</div>
 						<div className="porker-image">
-							<img className="porker-image-img" src={hog.image} alt={hog.name} />
-						</div>
+					  </div>
 					</div>
 				))}
-			</div>	
+			</div>
+			{porkers.length === 0 && <div className="no-porkers">No Porkers Found</div>}
 		</div>
 	);
 }
