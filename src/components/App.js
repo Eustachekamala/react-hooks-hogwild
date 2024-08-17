7e05936275ae7aad9a72f311140336c78a84589b
eustachekamala <eustachejared25@gmail.com>
2024-08-17 10:26:32 +0300

    I rendered the list of porgs

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,22 @@ import Nav from "./Nav";
 import hogs from "../porkers_data";
 
 function App() {
+
 	return (
 		<div className="App">
 			<Nav />
+			<div className="porkers">
+				{hogs.map((hog, index) => (
+					<div key={index} className="porker">
+						<div className="porker-name">{hog.name}</div>
+						<div className="porker-hogs">{hog.hogs}</div>
+						<div className="porker-price">{hog.price}</div>
+						<div className="porker-image">
+							<img className="porker-image-img" src={hog.image} alt={hog.name} />
+						</div>
+					</div>
+				))}
+			</div>	
 		</div>
 	);
 }
